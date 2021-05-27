93646dd61fa1394c0ceb32352cfcd94715355fa0
despencer <despencer@mail.ru>
2021-05-27 10:01:33 +0300

    Multirow walls also work

--- models/wall.py
+++ models/wall.py
@@ -4,17 +4,19 @@ import sys
 from trans import Transform
 from ldraw import Library, Item, Compound
 
-def createrow(color, sizex, sizey):
-    model = Compound()
+def createrow(model, color, sizex, sizey, z):
+    if model == None:
+        model = Compound()
     xy = Transform.rotatexy()
     for x in range(0, sizex):
-        model.addbrick('Brick 1 x 4', x*4, 0, 0, color=2)
-        model.addbrick('Brick 1 x 4', x*4+1, sizey*4, 0, color=3)
+        model.addbrick('Brick 1 x 4', x*4, 0, z, color=2)
+        model.addbrick('Brick 1 x 4', x*4+1, sizey*4, z, color=3)
     for y in range(0, sizey):
-        model.addbrick('Brick 1 x 4', 0, y*4+1, 0, color=4, rotation=xy)
-        model.addbrick('Brick 1 x 4', sizex*4, y*4, 0, color=5, rotation=xy)
+        model.addbrick('Brick 1 x 4', 0, y*4+1, z, color=4, rotation=xy)
+        model.addbrick('Brick 1 x 4', sizex*4, y*4, z, color=5, rotation=xy)
     return model
 
 Library.init()
-model = createrow(2, 2, 2)
+model = createrow(None, 2, 2, 2, 0)
+createrow(model, 2, 2, 2, 1)
 model.emitldraw(sys.stdout)
