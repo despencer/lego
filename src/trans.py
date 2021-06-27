f4f6639f9902d3385105ed11a2902e5850398202
despencer <despencer@mail.ru>
2021-06-27 22:31:12 +0300

    xy 3 times rotation (same as -xy) added.

--- src/trans.py
+++ src/trans.py
@@ -86,6 +86,15 @@ class Transform:
         return trans
 
     @classmethod
+    def rotatexy3(cls):
+        trans = cls()
+        trans.matrix = [ [ 0, 1, 0, 0 ],
+                         [ -1, 0, 0, 0 ],
+                         [ 0, 0, 1, 0 ],
+                         [ 0, 0, 0, 1] ]
+        return trans
+
+    @classmethod
     def flipy(cls):
         trans = cls()
         trans.matrix = [ [ 1, 0, 0, 0 ],
